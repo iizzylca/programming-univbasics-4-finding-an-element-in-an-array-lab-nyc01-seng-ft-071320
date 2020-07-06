f6245aa776f746b1d4efd82ff0f54bac9151a40e
Israel Canessa <israellcanessa@gmail.com>
2020-07-06 11:40:14 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,7 +2,6 @@ require 'pry'
 
 def find_element_index(array, value_to_find)
   count = 0
-  found_value_index = nil
   while count < array.length do
     if array[count] == value_to_find
       found_value_index = count
